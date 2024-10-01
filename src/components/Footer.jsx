beb7916e3091a585c2e2df7dd7b58a9efb6a9b13
mr-nishaant <nishant01210@gmail.com>
2024-10-01 13:21:04 +0530

    some change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,11 +21,11 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
             <div className="flex space-x-4 text-blue-700 ">
-              <SiGmail size={24} className=" cursor-pointer" onClick={() => handleLinkClick("mailto:nishant01210@gmail.com")} />
-              <FaLinkedinIn size={24} className=" cursor-pointer" onClick={() => handleLinkClick("https://linkedin.com/in/nishaantk")}/>
-              <FaFacebook size={24} className=" cursor-pointer" onClick={() => handleLinkClick("https://www.facebook.com/nishant.nsit.in")}/>
-              <FaInstagram size={24} className=" cursor-pointer" onClick={() => handleLinkClick("https://www.instagram.com/mr.nishant.mishra")} />
-              <VscGithub className=" cursor-pointer" onClick={() => handleLinkClick("https://github.com/mr-nishaant")}/>
+              <SiGmail size={30} className=" cursor-pointer" onClick={() => handleLinkClick("mailto:nishant01210@gmail.com")} />
+              <FaLinkedinIn size={30} className=" cursor-pointer" onClick={() => handleLinkClick("https://linkedin.com/in/nishaantk")}/>
+              <FaFacebook size={30} className=" cursor-pointer" onClick={() => handleLinkClick("https://www.facebook.com/nishant.nsit.in")}/>
+              <FaInstagram size={30} className=" cursor-pointer" onClick={() => handleLinkClick("https://www.instagram.com/mr.nishant.mishra")} />
+              <VscGithub size={30} className=" cursor-pointer" onClick={() => handleLinkClick("https://github.com/mr-nishaant")}/>
 
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
